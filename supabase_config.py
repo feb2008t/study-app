c8a836e3eae7c8edfba904393a6752a98c780127
feb2008t <feb2008t@gmail.com>
2026-06-15 21:16:06 +0900

    Update supabase_config.py

--- supabase_config.py
+++ supabase_config.py
@@ -1,7 +1,7 @@
 from supabase import create_client
 
-SUPABASE_URL = "..."
-SUPABASE_KEY = "..."
+SUPABASE_URL = "https://emjbpmjgnspbjoomecsj.supabase.co"
+SUPABASE_KEY = "sb_publishable_XReVlpVCAbTvGsli5Ak7qQ_VPCM4FEO"
 
 supabase = create_client(
     SUPABASE_URL,
