3586741d7fddb68e322d44331cc2e6eb2482e9db
feb2008t <feb2008t@gmail.com>
2026-06-22 18:31:40 +0900

    login button

--- home.py
+++ home.py
@@ -1,6 +1,30 @@
 import streamlit as st
+from supabase_config import supabase
 
-st.title("学習管理アプリ")
-st.write("勉強時間を記録して、あとから確認できます。")
+st.title("ログインテスト")
 
-st.write("左のサイドバーからページを選んでください。")
+email = st.text_input("メールアドレス")
+password = st.text_input("パスワード", type="password")
+
+if st.button("新規登録"):
+    try:
+        response = supabase.auth.sign_up({
+            "email": email,
+            "password": password
+        })
+        st.success("登録成功")
+        st.write("user_id:", response.user.id)
+    except Exception as e:
+        st.error(e)
+
+if st.button("ログイン"):
+    try:
+        response = supabase.auth.sign_in_with_password({
+            "email": email,
+            "password": password
+        })
+        st.success("ログイン成功")
+        st.write("user_id:", response.user.id)
+
+    except Exception as e:
+        st.error(e)
